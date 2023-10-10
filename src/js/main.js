070fb554750636060701b5a784d564a06051256e
Vahan Hovhannisyan <vaa.hovanisyan@gmail.com>
2023-10-10 17:57:02 +0400

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+import '../scss/style.scss'
+import Accordion from './accordion.js'
+
+const accordion1 = new Accordion('accordion1',{
+  speed:3000,
+  turn: true,
+  });
+  const accordion2 = new Accordion('accordion2')
